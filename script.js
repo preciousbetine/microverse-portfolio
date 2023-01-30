fe8c27711387d72dd1081fa2fe8b79a87ac16699
Precious Betine <preciousbetine@gmail.com>
2023-01-30 16:38:24 +0100

    Fix linter errors

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ const navLinksMobile = document.querySelectorAll('.nav-link-mobile');
 
 menuButton.addEventListener('click', () => {
   mobileMenu.style.display = 'flex';
-})
+});
 
 closeButton.addEventListener('click', () => {
   mobileMenu.style.display = 'none';
-})
+});
 
 navLinksMobile.forEach((navLink) => {
   navLink.addEventListener('click', () => {
     mobileMenu.style.display = 'none';
-  })
-})
+  });
+});
