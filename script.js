b7de78c3c1e0ba30c66fe70aab8112e63941245c
Precious Betine <preciousbetine@gmail.com>
2023-01-31 04:16:36 +0100

    Make header resize according to page section present in viewport

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const navLinksDesktop = document.querySelectorAll('.nav-link');
 // work (portfolio), about, contact
 
 const header = document.querySelector('header');
+const introSection = document.querySelector('#intro-section');
 const workSection = document.querySelector('#work-section');
 const aboutSection = document.querySelector('#about-section');
 const contactSection = document.querySelector('#contact-section');
@@ -25,21 +26,26 @@ navLinksMobile.forEach((navLink) => {
   });
 });
 
-const intersectionObserver = new IntersectionObserver((entries, observer) => {
+const intersectionObserver = new IntersectionObserver((entries) => {
   const sections = ['work-section', 'about-section', 'contact-section'];
   entries.forEach((entry) => {
     const navItem = navLinksDesktop[sections.indexOf(entry.target.id)];
     if (entry.isIntersecting) {
-      navLinksDesktop.forEach((item) => item.classList.remove('active'));
-      navItem.classList.add('active');
-    } else {
-      navItem.classList.remove('active');
+      if (entry.target.id === 'intro-section') {
+        header.style.height = '100px';
+      } else {
+        navLinksDesktop.forEach((item) => item.classList.remove('active'));
+        navItem.classList.add('active');
+        header.style.height = '80px';
+      }
     }
   });
 }, {
-  rootMargin: '-100px',
+  // rootMargin: '-150px',
+  threshold: 0.2,
 });
 
+intersectionObserver.observe(introSection);
 intersectionObserver.observe(workSection);
 intersectionObserver.observe(aboutSection);
 intersectionObserver.observe(contactSection);
