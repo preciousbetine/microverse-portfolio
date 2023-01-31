9b8361ea8462b11167a486571f1f3cd4df3ad5ba
Precious Betine <preciousbetine@gmail.com>
2023-01-31 03:30:07 +0100

    Make navbar links indicate the current active section on the page

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const mobileMenu = document.querySelector('#mobileMenu');
 const closeButton = document.querySelector('#closeButton');
 const navLinksMobile = document.querySelectorAll('.nav-link-mobile');
 
+const navLinksDesktop = document.querySelectorAll('.nav-link');
+// work (portfolio), about, contact
+
+const header = document.querySelector('header');
+const workSection = document.querySelector('#work-section');
+const aboutSection = document.querySelector('#about-section');
+const contactSection = document.querySelector('#contact-section');
+
 menuButton.addEventListener('click', () => {
   mobileMenu.style.display = 'flex';
 });
@@ -16,3 +24,22 @@ navLinksMobile.forEach((navLink) => {
     mobileMenu.style.display = 'none';
   });
 });
+
+const intersectionObserver = new IntersectionObserver((entries, observer) => {
+  const sections = ['work-section', 'about-section', 'contact-section'];
+  entries.forEach((entry) => {
+    const navItem = navLinksDesktop[sections.indexOf(entry.target.id)];
+    if (entry.isIntersecting) {
+      navLinksDesktop.forEach((item) => item.classList.remove('active'));
+      navItem.classList.add('active');
+    } else {
+      navItem.classList.remove('active');
+    }
+  });
+}, {
+  rootMargin: '-100px',
+});
+
+intersectionObserver.observe(workSection);
+intersectionObserver.observe(aboutSection);
+intersectionObserver.observe(contactSection);
