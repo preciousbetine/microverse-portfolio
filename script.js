dbb2862cb68ed849c5dac9c27aeeb97376cf03f6
Precious Betine <preciousbetine@gmail.com>
2023-01-31 13:55:51 +0100

    Create portfolio object

--- script.js
+++ script.js
@@ -49,3 +49,51 @@ intersectionObserver.observe(introSection);
 intersectionObserver.observe(workSection);
 intersectionObserver.observe(aboutSection);
 intersectionObserver.observe(contactSection);
+
+
+const portfolioItems = [
+  {
+    name: 'Tonic',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    featuredImage: '',
+    technologies: ['html', 'css', 'javascript'],
+    liveLink: '',
+    sourceLink: '',
+    category: 'CANOPY',
+    stack: 'Back End Dev',
+    year: '2015',
+  },
+  {
+    name: 'Multi-Post Stories',
+    description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    featuredImage: '',
+    technologies: ['html', 'Ruby on rails', 'css', 'javascript'],
+    liveLink: '',
+    sourceLink: '',
+    category: 'FACEBOOK',
+    stack: 'Full Stack Dev',
+    year: '2015',
+  },
+  {
+    name: 'Facebook 360',
+    description: 'Exploring the future of media in Facebook\'s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+    featuredImage: '',
+    technologies: ['html', 'Ruby on rails', 'css', 'javascript'],
+    liveLink: '',
+    sourceLink: '',
+    category: 'FACEBOOK',
+    stack: 'Full Stack Dev',
+    year: '2015',
+  },
+  {
+    name: 'Uber Navigation',
+    description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    featuredImage: '',
+    technologies: ['html', 'Ruby on rails', 'css', 'javascript'],
+    liveLink: '',
+    sourceLink: '',
+    category: 'Uber',
+    stack: 'Lead Developer',
+    year: '2018',
+  },
+];
